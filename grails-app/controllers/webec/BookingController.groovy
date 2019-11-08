9fc5ab03e4aad9b62150600d2d83e47e89779ae2
yanick.businger@students.fhnw.ch <yanick.businger@students.fhnw.ch>
2019-11-08 12:35:27 +0100

    fix merge issue

--- grails-app/controllers/webec/BookingController.groovy
+++ grails-app/controllers/webec/BookingController.groovy
@@ -9,12 +9,5 @@ class BookingController {
         def result = Booking.findAllByBooker(booker)*.room
         render text: result.toString()
     }
-
-    def booker() {
-        def room = Room.findByName('1.313')
-        def result = Booking.findAllByRoom(room)*.booker.unique().lastName
-        render text: result.toString()
-    }
-
     static scaffold = Booking
 }
